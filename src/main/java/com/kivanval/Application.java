dcf9a68ad395ac8ff848fa469216840d2793b329
Kyrylov Ivan <kivanval@gmail.com>
2022-04-27 20:50:20 +0300

    Added greeting protocol

--- src/main/java/com/kivanval/Application.java
+++
@@ -1,7 +0,0 @@
-package com.kivanval;
-
-public class Application {
-    public static void main(String[] args) {
-        //TODO
-    }
-}
---
+++ src/main/java/com/kivanval/telegram/bots/TelegramBot.java
@@ -0,0 +1,46 @@
+package com.kivanval.telegram.bots;
+
+import com.vdurmont.emoji.EmojiParser;
+import org.telegram.abilitybots.api.bot.AbilityBot;
+import org.telegram.abilitybots.api.bot.BaseAbilityBot;
+import org.telegram.abilitybots.api.objects.Reply;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
+
+import static org.telegram.abilitybots.api.util.AbilityUtils.getChatId;
+
+public class TelegramBot extends AbilityBot {
+    private static final String BOT_TOKEN = System.getenv("BOT_TOKEN");
+    private static final String BOT_USERNAME = System.getenv("BOT_USERNAME");
+
+    public TelegramBot() {
+        super(BOT_TOKEN, BOT_USERNAME);
+    }
+
+    public TelegramBot(String token, String username) {
+        super(token, username);
+    }
+
+    public Reply sayGreetingProtocol() {
+        String message = EmojiParser.parseToUnicode("Героям слава! :heart::ua:");
+        BiConsumer<BaseAbilityBot, Update> action = (baseAbilityBot, update)
+                -> silent.send(message, getChatId(update));
+        return Reply.of(action, isGreetingProtocol());
+    }
+
+    private Predicate<Update> isGreetingProtocol() {
+        return (update)
+                -> update.getMessage().getText()
+                        .replaceAll("[\\s\\p{P}]+", " ")
+                        .strip()
+                        .matches("^[^\\p{L}]*[Сс][Лл][Аа][Вв][Аа] У[Кк][Рр][Аа][Її][Нн][Іі][^\\p{L}]*$");
+    }
+
+
+    @Override
+    public long creatorId() {
+        return 932231787;
+    }
+}
