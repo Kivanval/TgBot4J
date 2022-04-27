dce2761dfb529e4ee68da6404bddc7fe1fc0fc82
Kyrylov Ivan <kivanval@gmail.com>
2022-04-27 23:32:02 +0300

    Added toggles

--- src/main/java/com/kivanval/telegram/bots/TelegramBot.java
+++ src/main/java/com/kivanval/telegram/bots/TelegramBot.java
@@ -4,6 +4,8 @@ import com.vdurmont.emoji.EmojiParser;
 import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.abilitybots.api.bot.BaseAbilityBot;
 import org.telegram.abilitybots.api.objects.Reply;
+import org.telegram.abilitybots.api.toggle.AbilityToggle;
+import org.telegram.abilitybots.api.toggle.BareboneToggle;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.function.BiConsumer;
@@ -15,12 +17,18 @@ public class TelegramBot extends AbilityBot {
     private static final String BOT_TOKEN = System.getenv("BOT_TOKEN");
     private static final String BOT_USERNAME = System.getenv("BOT_USERNAME");
 
+    private static final BareboneToggle TOGGLE = new BareboneToggle();
+
     public TelegramBot() {
-        super(BOT_TOKEN, BOT_USERNAME);
+        super(BOT_TOKEN, BOT_USERNAME, TOGGLE);
     }
 
     public TelegramBot(String token, String username) {
-        super(token, username);
+        super(token, username, TOGGLE);
+    }
+
+    public TelegramBot(String token, String username, AbilityToggle toggle) {
+        super(token, username, toggle);
     }
 
     public Reply sayGreetingProtocol() {
@@ -35,11 +43,10 @@ public class TelegramBot extends AbilityBot {
                 -> update.hasMessage()
                 && update.getMessage().hasText()
                 && update.getMessage().getText()
-                        .replaceAll("[\\s\\p{P}]+", " ")
-                        .strip()
-                        .matches("^[^\\p{L}]*[Сс][Лл][Аа][Вв][Аа] У[Кк][Рр][Аа][Її][Нн][Іі][^\\p{L}]*$");
+                .replaceAll("[\\s\\p{P}]+", " ")
+                .strip()
+                .matches("^[^\\p{L}]*[Сс][Лл][Аа][Вв][Аа] У[Кк][Рр][Аа][Її][Нн][Іі][^\\p{L}]*$");
     }
-
 
     @Override
     public long creatorId() {
